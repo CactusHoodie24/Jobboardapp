9deb3ae97ead275f13aaa7697bb92deaa6d7a13f
CactusHoodie24 <preciousphimba@gmail.com>
2025-06-11 20:52:11 +0200

    a step to improved form validation

--- app/dashboard/employer/applicant/[id]/page.tsx
+++ app/dashboard/employer/applicant/[id]/page.tsx
@@ -1,18 +1,25 @@
 import { prisma } from '@/prisma'
 import React from 'react'
 
-const ApplicantDetail = async ({params}: {params: {id: string}}) => {
-    const applicationId = Number(params.id)
-     const jobs = await prisma.application.findUnique({
-        where: {id : applicationId}
-     })
-  return (
-    <div key={jobs?.id}>
-     <h2>{jobs?.status}</h2>
-     <h2>{jobs?.applicantId}</h2>
-     <h2>{jobs?.applicantId}</h2>
-    </div>
-  )
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default ApplicantDetail
+const ApplicantDetail = async ({ params }: PageProps) => {
+  const applicationId = Number(params.id);
+  
+  const job = await prisma.application.findUnique({
+    where: { id: applicationId }
+  });
+
+  return (
+    <div key={job?.id}>
+      <h2>{job?.status}</h2>
+      <h2>{job?.applicantId}</h2>
+    </div>
+  );
+};
+
+export default ApplicantDetail;
